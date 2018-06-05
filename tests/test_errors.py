27e6d0e4e33267e233180feb3cfec0fda8ff6bc8
RJ722 <rahul722j@gmail.com>
2018-06-05 14:23:16 +0530

    Ensure error for non UTF8 encoded files

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,3 +1,6 @@
+import codecs
+import tempfile
+
 import pytest
 
 from . import v
@@ -21,3 +24,18 @@ def foo():
 """)
     with pytest.raises(ValueError):
         v.get_unused_code(min_confidence=150)
+
+
+def test_non_utf8_encoding(v):
+    code = """\
+def foo():
+    pass
+
+foo()
+"""
+    with tempfile.NamedTemporaryFile(mode='wb', suffix='.py') as f:
+        f.write(codecs.BOM_UTF16_LE)
+        f.write(code.encode('utf_16_le'))
+        f.flush()  # Ensure that the file is actually written to disk.
+        v.scavenge([f.name])
+    assert v.found_dead_code_or_error
