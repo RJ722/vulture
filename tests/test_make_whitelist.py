6b93104ee1f646097f03052b911011d7112f8945
RJ722 <rahul722j@gmail.com>
2018-06-27 13:36:00 +0530

    No need to pass fixture objects from every test

--- tests/test_make_whitelist.py
+++ tests/test_make_whitelist.py
@@ -1,25 +1,29 @@
 import subprocess
 
+import pytest
 from vulture.make_whitelist import make_whitelist
 
 from . import v
 assert v  # Silence pyflakes.
 
 
-def check_whitelist(code, expected_output, v, tmpdir, capsys):
-    sample = str(tmpdir.join("unused_code.py"))
-    xml = str(tmpdir.join("coverage.xml"))
-    with open(sample, 'w') as f:
-        f.write(code)
-    subprocess.call(["coverage", "run", sample])
-    subprocess.call(["coverage", "xml", "-o", xml])
-    v.scavenge([sample])
-    capsys.readouterr()  # Flush output from coverage run 
-    make_whitelist(v, xml)
-    assert capsys.readouterr().out == expected_output.format(sample)
+@pytest.fixture
+def check_whitelist(v, tmpdir, capsys):
+    def check(code, expected_output):
+        sample = str(tmpdir.join("unused_code.py"))
+        xml = str(tmpdir.join("coverage.xml"))
+        with open(sample, 'w') as f:
+            f.write(code)
+        subprocess.call(["coverage", "run", sample])
+        subprocess.call(["coverage", "xml", "-o", xml])
+        v.scavenge([sample])
+        capsys.readouterr()  # Flush output from coverage run
+        make_whitelist(v, xml)
+        assert capsys.readouterr().out == expected_output.format(sample)
+    return check
 
 
-def test_create_whitelist(v, tmpdir, capsys):
+def test_create_whitelist(check_whitelist):
     code = """\
 class Greeter:
     def greet(self):
@@ -33,4 +37,4 @@ greet_func()
 greet
 
 """
-    check_whitelist(code, expected_output, v, tmpdir, capsys)
+    check_whitelist(code, expected_output)
