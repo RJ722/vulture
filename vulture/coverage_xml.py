dab1b12127aa76c9b4893782ebd581d4f8086905
RJ722 <rahul722j@gmail.com>
2018-07-08 22:56:49 +0530

    debug windows

--- vulture/coverage_xml.py
+++ vulture/coverage_xml.py
@@ -1,4 +1,4 @@
-import os.path
+import posixpath
 from xml.etree import ElementTree as ET
 
 from vulture import utils
@@ -6,14 +6,19 @@ from vulture import utils
 
 def parse_coverage_xml(xml):
     with open(xml) as f:
-        tree = ET.parse(f)
-    return tree
+        content = f.read().lower()
+        return ET.fromstring(content)
 
 
 def detect_used_funcs(v, xml):
     tree = parse_coverage_xml(xml)
+    print("The XML file: ")
+    with open(xml, 'r') as f:
+        print(f.read().lower())
     for item in v.unused_funcs:
-        filename = os.path.normcase(utils.format_path(item.filename))
+        filename = posixpath.join(*utils.format_path(
+            item.filename).lower().split('\\'))
+        print("filename: ", filename)
         xpath = ('./packages/package/classes/class/[@filename="{}"]'
                  '/lines/line[@hits="1"]'.format(filename))
         lines_hit = [int(
