ed625561d99870ef41ac1468f34fee1420343cca
Rahul Jha <rahul@deepsource.io>
2020-05-25 10:07:34 +0530

    Create really.py

---
+++ really.py
@@ -0,0 +1,8 @@
+import this
+
+
+from __future__ import print_function
+
+
+def main():
+  print("Hello World, this is a really really reallyyyyyyy long line. Do you think I'm kidding? No, this is in fact a really really reallyyyyyyy long line!! ;)")
